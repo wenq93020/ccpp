f86951fa598c59146605362906e309e16fbbf0dd
Derktion <wenq93020@gmail.com>
2022-05-27 21:20:42 +0800

    Chapter3

---
+++ Code/Chapter3/01.function_basics.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//
+//double F(double x) {
+//  return x * x + x + 1;
+//}
+//
+//double G(double x, double y, double z) {
+//  return x * x + y * y + z * z;
+//}
+//
+//int main(void) {
+//
+//  puts("Hello World");
+//
+//  double result_f = F(10);
+//  double result_g = G(10, 11, 12);
+//
+//  printf("%f\n", result_f);
+//  printf("%f\n", result_g);
+//
+//  return 0;
+//}
+//
+//
+//
---
+++ Code/Chapter3/02.porto_type.c
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//
+//void EmptyParamList(void);
+//
+//int Add(int left, int right);
+//
+//int main(void){
+//  puts("");
+//  EmptyParamList();
+//
+//  int result = Add(1,2);
+//  printf("%d\n", result);
+//
+//  return 0;
+//}
+//
+//
+//void EmptyParamList(void){
+//
+//  puts("Hello");
+//
+//}
+//
+//int Add(int left, int right){
+//  return left + right;
+//}
+//
