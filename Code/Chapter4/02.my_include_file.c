ea903b0018cb034c1369cca23d83b526c8f6d0d2
Derktion <wenq93020@gmail.com>
2022-06-05 17:09:03 +0800

    Chapter4

---
+++ Code/Chapter4/02.my_include_file.c
@@ -0,0 +1,9 @@
+//#include<stdio.h>
+//#include<factorial.h>
+//
+//int main(void){
+//  printf("3! = %d\n", Factorial(3));
+//
+//  return 0;
+//}
+//
---
+++ Code/Chapter4/include/factorial.h
@@ -0,0 +1,8 @@
+#ifndef CHAPTER4_INCLUDE_FACTORIAL_H_
+#define CHAPTER4_INCLUDE_FACTORIAL_H_
+
+unsigned int Factorial(unsigned int n );
+
+unsigned int FactorialByInteration(unsigned int n);
+
+#endif //CHAPTER4_INCLUDE_FACTORIAL_H_
